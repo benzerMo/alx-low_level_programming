ec0ed18b0d21612de53e1391b1f09498f0f602f8
benzerMo <mohamed.benzeroual.github@gmail.com>
2023-09-08 19:30:06 -0700

    more c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-    int first_digit;
-    int second_digit;
+	int first_digit;
+	int second_digit;
 
-    for (first_digit = 0; first_digit < 9; first_digit++)
-    {
-        for (second_digit = first_digit + 1; second_digit < 10; second_digit++)
-        {
-            putchar('0' + first_digit);
-            putchar('0' + second_digit);
+	for (first_digit = 0; first_digit < 9; first_digit++)
+	{
+		for (second_digit = first_digit + 1; second_digit < 10; second_digit++)
+		{
+			putchar('0' + first_digit);
+			putchar('0' + second_digit);
 
-            if (first_digit != 8 || second_digit != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (first_digit != 8 || second_digit != 9)
+			{
+				putchar(',');
+				putchar('\t');
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
